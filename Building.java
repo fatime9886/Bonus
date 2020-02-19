cbe210d04995cd62bf2de97062335489dfc7b602
Фатиме Т <59640207+fatime9886@users.noreply.github.com>
2020-02-18 22:53:04 -0800

    Add files via upload

---
+++ Building.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+
+public class Building {
+	
+	
+	 public static void main(String[] args) {
+	     
+		 int height[] = { 3, 5, 1, 2, 3 }; 
+		    int colour[] = { 1, 2, 3, 4, 3 }; 
+	    
+	    { 
+	        int arr[] = {7, 4, 8, 2, 9}; 
+	          
+	        System.out.println(countBuildings(arr, arr.length)); 
+	          
+	    }
+}
+
+
+
+	private static char[] countBuildings(int[] arr, int length) {
+		// TODO Auto-generated method stub
+		 return null;
+	}
+	
+   
+
+	} 
+	
